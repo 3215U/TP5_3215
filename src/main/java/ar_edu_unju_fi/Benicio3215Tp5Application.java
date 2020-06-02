8ea35ed7406508da9d806afd841a0954435e8c8c
Pc <homecuenta8gmail.com>
2020-06-02 14:50:36 -0300

    Asunto: Desarrollo del proyecto
    Responsable: Roxana
    Taks00: Desarrollo del proyecto
    Consigna: desarrollo de consigna hasta el punto 1 y 2 del tp5 gitHub

---
+++ src/main/java/ar_edu_unju_fi/Benicio3215Tp5Application.java
@@ -0,0 +1,13 @@
+package ar_edu_unju_fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Benicio3215Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Benicio3215Tp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar_edu_unju_fi/Benicio3215Tp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar_edu_unju_fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Benicio3215Tp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
